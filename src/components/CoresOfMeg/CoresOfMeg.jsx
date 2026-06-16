8953c15deec5ccc8f7125d3888c9749d3aa69547
Zain Saleem <zain.saleem155@gmail.com>
2026-06-16 14:13:10 +0500

    updated

--- src/components/CoresOfMeg/CoresOfMeg.jsx
+++ src/components/CoresOfMeg/CoresOfMeg.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { FaTruck, FaMoneyBillWave, FaHeadset, FaCheckCircle } from 'react-icons/fa';
 import './CoresOfMeg.css';
 
-import coresImage from '../../assets/cores-of-meg/cores-of-meg.png';
+import coresImage from '../../assets/cores.jpg';
 
 const highlights = [
   { Icon: FaTruck, text: 'Truck Dispatch Services' },
